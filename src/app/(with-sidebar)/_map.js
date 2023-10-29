daa07ded7d5cc93a44bffa873106527462a1421c
Alphonsus Jovian J.R <55451337+jovianjr@users.noreply.github.com>
2023-10-30 00:43:04 +0700

    feat(sidebar): smoothing sidebar (#6)

--- src/app/(with-sidebar)/_map.js
+++ src/app/(with-sidebar)/_map.js
@@ -1,6 +1,6 @@
 'use client'
 import 'leaflet/dist/leaflet.css'
-import { MapContainer, TileLayer } from 'react-leaflet'
+import { MapContainer, TileLayer, ZoomControl } from 'react-leaflet'
 
 export default function Home() {
 	const initialPosition = [-2, 112]
@@ -10,6 +10,7 @@ export default function Home() {
 				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 			/>
+			<ZoomControl position="topright" />
 		</MapContainer>
 	)
 }
