6293debe32f167aa4cbd4b18d01034f371dfa27a
Alphonsus Jovian J.R <55451337+jovianjr@users.noreply.github.com>
2023-10-30 01:28:20 +0700

    Video replay (#7)
    * hotfix: tooltip
    
    * hotfix: table in replay pag
    
    * feat: try get frame image
    
    * hotfix(sidebar): fix link
    
    * hotfix: add kotak
    
    * fix: prediction kotak
    
    * fix: bug on video click
    
    * delete
    
    * fix: bug kotak tidk muncul
    
    * Update _sidebar.js
    
    * Update _sidebar.js
    
    ---------
    
    Co-authored-by: clstyn <fiorenzacelestyn@gmail.com>

--- src/app/components/IconComponents.jsx
+++ src/app/components/IconComponents.jsx
@@ -1,9 +1,43 @@
-import React from "react";
+'use client'
 
-export default function IconComponent({ icon, name }) {
-  return (
-    <div className="flex items-center justify-center cursor-pointer">
-      {icon}
-    </div>
-  );
+import React, { useState, useEffect } from 'react'
+import { Tooltip } from 'react-tooltip'
+import 'react-tooltip/dist/react-tooltip.css'
+
+export default function IconComponent({ icon, name, onClick = null }) {
+	const [isMounted, setIsMounted] = useState(false)
+
+	useEffect(() => {
+		setIsMounted(true)
+	}, [])
+
+	return (
+		isMounted && (
+			<>
+				<div className="flex items-center justify-center">
+					<div
+						data-tooltip-content={name}
+						data-tooltip-id="icon-tooltip"
+						className="cursor-pointer"
+						onClick={onClick}
+					>
+						{icon}
+					</div>
+				</div>
+
+				<Tooltip
+					id="icon-tooltip"
+					place="bottom"
+					style={{
+						backgroundColor: '#E0E0E0',
+						color: '#222222',
+						borderRadius: '8px',
+						fontSize: '16px',
+						padding: '4px 12px',
+					}}
+					noArrow
+				/>
+			</>
+		)
+	)
 }
