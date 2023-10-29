ae46eaafb90bfa5432f69f208837c004bc48964b
Jovian <alphonsusjovian@gmail.com>
2023-10-29 23:22:42 +0700

    feat(map): add map package

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,22 @@
 'use client'
+import 'leaflet/dist/leaflet.css'
+import { MapContainer, TileLayer } from 'react-leaflet'
+
+const initialPosition = [-2, 112]
 
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<div className="h-20">Homepage</div>
+		<main className="flex min-h-screen flex-col items-center justify-between">
+			<MapContainer
+				className="h-screen w-full"
+				center={initialPosition}
+				zoom={6}
+			>
+				<TileLayer
+					attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+				/>
+			</MapContainer>
 		</main>
 	)
 }
