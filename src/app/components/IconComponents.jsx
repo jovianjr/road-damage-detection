4e6ad2539565bd7d4905feef304f80136c79abb0
clstyn <fiorenzacelestyn@gmail.com>
2023-10-29 14:35:28 +0700

    hotfix: tooltip

--- src/app/components/IconComponents.jsx
+++ src/app/components/IconComponents.jsx
@@ -1,9 +1,38 @@
-import React from "react";
+"use client";
+
+import React, { useState, useEffect } from "react";
+import { Tooltip } from "react-tooltip";
+import "react-tooltip/dist/react-tooltip.css";
 
 export default function IconComponent({ icon, name }) {
+  const [isMounted, setIsMounted] = useState(false); // Need this for the react-tooltip
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
   return (
-    <div className="flex items-center justify-center cursor-pointer">
-      {icon}
-    </div>
+    isMounted && (
+      <>
+        <div className="flex items-center justify-center cursor-pointer">
+          <div data-tooltip-content={name} data-tooltip-id="icon-tooltip">
+            {icon}
+          </div>
+        </div>
+
+        <Tooltip
+          id="icon-tooltip"
+          place="bottom"
+          style={{
+            backgroundColor: "#E0E0E0",
+            color: "#222222",
+            borderRadius: "8px",
+            fontSize: "16px",
+            padding: "4px 12px",
+          }}
+          noArrow
+        />
+      </>
+    )
   );
 }
