942e6f9040569d0a85cd4690232edeea98680865
Jovian <alphonsusjovian@gmail.com>
2023-10-31 22:05:55 +0700

    feat(api): setup minio for upload file

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	env: {
+		MINIO_ENDPOINT: process.env.MINIO_ENDPOINT,
+		MINIO_PORT: process.env.MINIO_PORT,
+		MINIO_SSL: process.env.MINIO_SSL,
+	},
 	async rewrites() {
 		return [
 			{
-				source: '/api/auth/:path*',
-				destination: '/api/auth/:path*',
+				source: '/api/upload-file',
+				destination: '/api/upload-file',
 			},
 			{
 				source: '/api/:path*',
