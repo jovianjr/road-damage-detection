8381f46dea30193d1b932506e2dec05037059f3f
clstyn <fiorenzacelestyn@gmail.com>
2023-11-09 16:06:11 +0700

    responsive popup

--- src/app/(with-sidebar)/rekaman/[id]/_deletePopup.js
+++ src/app/(with-sidebar)/rekaman/[id]/_deletePopup.js
@@ -30,7 +30,7 @@ const DeletePopup = ({ isDeleting, idData, titleData, onClose }) => {
 				isDeleting ? '' : 'hidden'
 			)}
 		>
-			<div className="container mx-auto flex h-full w-1/2 items-center justify-center">
+			<div className="container mx-auto flex h-full w-1/2 items-center justify-center max-md:text-sm">
 				<div className="relative flex w-[500px] flex-col rounded-xl bg-white p-8 text-black">
 					<button
 						onClick={onClose}
@@ -38,7 +38,9 @@ const DeletePopup = ({ isDeleting, idData, titleData, onClose }) => {
 					>
 						<X className="mx-auto w-8" />
 					</button>
-					<h1 className="mb-4 text-2xl font-semibold">Konfirmasi Hapus</h1>
+					<h1 className="mb-4 text-2xl font-semibold max-md:text-lg">
+						Konfirmasi Hapus
+					</h1>
 					<p>Anda yakin ingin menghapus rekaman {titleData}?</p>
 					<div className="flex justify-center gap-4">
 						<Button
