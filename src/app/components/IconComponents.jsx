7cffd452432353e01298f72f038c68884f49841e
Alphonsus Jovian J.R <55451337+jovianjr@users.noreply.github.com>
2023-11-10 00:07:41 +0700

    Hotfix/tooltip (#22)
    * hotfix(icon-component): fix tooltip
    
    * hotfix(detail-rekaman): fix tooltip biar ga rebutan

--- src/app/components/IconComponents.jsx
+++ src/app/components/IconComponents.jsx
@@ -1,44 +1,24 @@
 'use client'
 
-import React, { useState, useEffect } from 'react'
-import { Tooltip } from 'react-tooltip'
+import React from 'react'
 import 'react-tooltip/dist/react-tooltip.css'
 
-export default function IconComponent({ icon, name, onClick = null }) {
-	const [isMounted, setIsMounted] = useState(false)
-
-	useEffect(() => {
-		setIsMounted(true)
-	}, [])
-
+export default function IconComponent({
+	id = 'icon-tooltip',
+	icon,
+	name,
+	onClick = null,
+}) {
 	return (
-		isMounted && (
-			<>
-				<div className="flex items-center justify-center max-md:hidden max-md:p-2">
-					<div
-						data-tooltip-content={name}
-						data-tooltip-id="icon-tooltip"
-						className="cursor-pointer"
-						onClick={onClick}
-					>
-						{icon}
-					</div>
-				</div>
-
-				<Tooltip
-					id="icon-tooltip"
-					place="bottom"
-					style={{
-						backgroundColor: '#E0E0E0',
-						color: '#222222',
-						borderRadius: '8px',
-						fontSize: '16px',
-						padding: '4px 12px',
-					}}
-					noArrow
-					className="max-md:hidden"
-				/>
-			</>
-		)
+		<div className="flex items-center justify-center max-md:hidden max-md:p-2">
+			<div
+				data-tooltip-content={name}
+				data-tooltip-id={id}
+				className="cursor-pointer"
+				onClick={onClick}
+			>
+				{icon}
+			</div>
+		</div>
 	)
 }
