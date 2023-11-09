28b6645ff6c199bda6d558c507dbf52255b3c330
Jovian <alphonsusjovian@gmail.com>
2023-11-09 23:52:08 +0700

    hotfix(icon-component): fix tooltip

--- src/app/(with-sidebar)/layout.js
+++ src/app/(with-sidebar)/layout.js
@@ -1,6 +1,7 @@
 import Sidebar from '@/app/(with-sidebar)/_sidebar'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+import 'react-tooltip/dist/react-tooltip.css'
 
 export default function withSidebarLayout({ children }) {
 	return (
--- src/app/(with-sidebar)/rekaman/page.js
+++ src/app/(with-sidebar)/rekaman/page.js
@@ -11,9 +11,10 @@ import {
 	Fullscreen,
 	FileDown,
 } from 'lucide-react'
+import { useQuery } from 'react-query'
+import { Tooltip } from 'react-tooltip'
 
 import IconComponent from '@/app/components/IconComponents'
-import { useQuery } from 'react-query'
 import { getAllRoad, getSingleRoadCsv } from '@/utils/services/road'
 
 const headerTableContent = [
@@ -75,22 +76,77 @@ export default function DaftarRekaman() {
 	}
 
 	return (
-		<div className="justify-starts mt-4 flex min-h-screen w-full flex-col items-center bg-[#fff] text-black md:mt-16">
-			<h1 className="mb-2 w-full text-left text-xl font-semibold max-md:px-4 md:mb-8 md:w-3/4 md:text-2xl">
-				Daftar Rekaman
-			</h1>
+		<>
+			<div className="justify-starts mt-4 flex min-h-screen w-full flex-col items-center bg-[#fff] text-black md:mt-16">
+				<h1 className="mb-2 w-full text-left text-xl font-semibold max-md:px-4 md:mb-8 md:w-3/4 md:text-2xl">
+					Daftar Rekaman
+				</h1>
 
-			{roadListIsLoading || roadListIsFetching ? (
-				<>
-					<div className="container mx-auto">
-						<div className="flex justify-center max-md:hidden">
-							<table className="w-full bg-white text-lg font-medium max-md:text-sm">
-								<thead>
-									<tr>
-										{headerTableContent.map((item) => (
+				{roadListIsLoading || roadListIsFetching ? (
+					<>
+						<div className="container mx-auto">
+							<div className="flex justify-center max-md:hidden">
+								<table className="w-full bg-white text-lg font-medium max-md:text-sm">
+									<thead>
+										<tr>
+											{headerTableContent.map((item) => (
+												<td
+													className={clsx(
+														'border-b py-2 text-center max-md:px-2 md:py-4',
+														item !==
+															headerTableContent[headerTableContent.length - 1]
+															? 'border-r'
+															: ''
+													)}
+													key={item.id}
+												>
+													<div className="flex items-center justify-center gap-4">
+														{item.icon}
+														<p>{item.name}</p>
+													</div>
+												</td>
+											))}
+										</tr>
+									</thead>
+
+									<tbody>
+										{Array(10)
+											.fill()
+											.map((_, id) => (
+												<tr key={id}>
+													{headerTableContent.map((item) => (
+														<td key={item.id}>
+															<div className="m-2 h-[48px] animate-pulse rounded-lg bg-slate-300 p-2"></div>
+														</td>
+													))}
+												</tr>
+											))}
+									</tbody>
+								</table>
+							</div>
+						</div>
+
+						<div className="mt-8 flex w-full flex-col gap-4 px-4 md:hidden">
+							{Array(10)
+								.fill()
+								.map((_, id) => (
+									<div
+										key={id}
+										className="m-2 h-24 animate-pulse rounded-lg bg-slate-300 p-2"
+									></div>
+								))}
+						</div>
+					</>
+				) : (
+					<>
+						<table className="w-3/4 bg-white text-lg font-medium max-md:hidden">
+							<thead>
+								<tr>
+									{headerTableContent.map((item) => {
+										return (
 											<td
 												className={clsx(
-													'border-b py-2 text-center max-md:px-2 md:py-4',
+													'border-b py-2 text-center md:py-4',
 													item !==
 														headerTableContent[headerTableContent.length - 1]
 														? 'border-r'
@@ -103,149 +159,115 @@ export default function DaftarRekaman() {
 													<p>{item.name}</p>
 												</div>
 											</td>
-										))}
-									</tr>
-								</thead>
+										)
+									})}
+								</tr>
+							</thead>
 
-								<tbody>
-									{Array(10)
-										.fill()
-										.map((_, id) => (
-											<tr key={id}>
-												{headerTableContent.map((item) => (
-													<td key={item.id}>
-														<div className="m-2 h-[48px] animate-pulse rounded-lg bg-slate-300 p-2"></div>
-													</td>
-												))}
-											</tr>
-										))}
-								</tbody>
-							</table>
-						</div>
-					</div>
-
-					<div className="mt-8 flex w-full flex-col gap-4 px-4 md:hidden">
-						{Array(10)
-							.fill()
-							.map((_, id) => (
-								<div
-									key={id}
-									className="m-2 h-24 animate-pulse rounded-lg bg-slate-300 p-2"
-								></div>
-							))}
-					</div>
-				</>
-			) : (
-				<>
-					<table className="w-3/4 bg-white text-lg font-medium max-md:hidden">
-						<thead>
-							<tr>
-								{headerTableContent.map((item) => {
-									return (
-										<td
-											className={clsx(
-												'border-b py-2 text-center md:py-4',
-												item !==
-													headerTableContent[headerTableContent.length - 1]
-													? 'border-r'
-													: ''
-											)}
-											key={item.id}
-										>
-											<div className="flex items-center justify-center gap-4">
-												{item.icon}
-												<p>{item.name}</p>
-											</div>
+							<tbody>
+								{roadList?.data !== 0 ? (
+									roadList?.data?.map((item, index) => (
+										<tr key={index}>
+											<td className="border-r py-2 text-center md:py-4">
+												{convertTime(item.createdAt)}
+											</td>
+											<td className="border-r py-2 text-center md:py-4">
+												{item.title}
+											</td>
+											<td className="border-r py-2 text-center md:py-4">
+												{item.detectionMeta.totalDamage}
+											</td>
+											<td className="flex gap-2 border-r px-4 py-2 text-center md:py-4">
+												{item.videoData?.map((data, index) => {
+													return (
+														<div
+															className="rounded-lg bg-pink-300 px-2.5 py-1 text-xl"
+															key={index}
+														>
+															{data.text}
+														</div>
+													)
+												})}
+											</td>
+											<td className="px-3 py-2 text-center md:py-4">
+												<div className="grid w-full grid-cols-2">
+													<Link
+														href={`/rekaman/${item._id}`}
+														className="cursor-pointer"
+													>
+														<IconComponent
+															icon={<Fullscreen />}
+															name="Lihat Replay"
+														/>
+													</Link>
+													<div
+														className="cursor-pointer"
+														onClick={() =>
+															handleDownloadCsv(item._id, item.title)
+														}
+													>
+														<IconComponent
+															icon={<FileDown />}
+															name="Unduh CSV"
+														/>
+													</div>
+												</div>
+											</td>
+										</tr>
+									))
+								) : (
+									<tr>
+										<td className="py-2 text-center text-2xl italic md:py-4">
+											Tidak ada data ditemukan
 										</td>
-									)
-								})}
-							</tr>
-						</thead>
-
-						<tbody>
+									</tr>
+								)}
+							</tbody>
+						</table>
+						{/* card for small screen  */}
+						<div className="mt-8 flex w-full flex-col gap-4 px-4 md:hidden">
 							{roadList?.data !== 0 ? (
 								roadList?.data?.map((item, index) => (
-									<tr key={index}>
-										<td className="border-r py-2 text-center md:py-4">
-											{convertTime(item.createdAt)}
-										</td>
-										<td className="border-r py-2 text-center md:py-4">
-											{item.title}
-										</td>
-										<td className="border-r py-2 text-center md:py-4">
+									<div
+										className="flex flex-col gap-1 rounded-md border border-c-blue p-4 shadow-md"
+										key={index}
+									>
+										<p>{convertTime(item.createdAt)}</p>
+										<p className="w-full text-lg font-semibold">{item.title}</p>
+										<p>
+											<span>Jumlah kerusakan: </span>
 											{item.detectionMeta.totalDamage}
-										</td>
-										<td className="flex gap-2 border-r px-4 py-2 text-center md:py-4">
-											{item.videoData?.map((data, index) => {
-												return (
-													<div
-														className="rounded-lg bg-pink-300 px-2.5 py-1 text-xl"
-														key={index}
-													>
-														{data.text}
-													</div>
-												)
-											})}
-										</td>
-										<td className="px-3 py-2 text-center md:py-4">
-											<div className="grid w-full grid-cols-2">
-												<Link
-													href={`/rekaman/${item._id}`}
-													className="cursor-pointer"
-												>
-													<IconComponent
-														icon={<Fullscreen />}
-														name="Lihat Replay"
-													/>
-												</Link>
-												<div
-													className="cursor-pointer"
-													onClick={() =>
-														handleDownloadCsv(item._id, item.title)
-													}
-												>
-													<IconComponent icon={<FileDown />} name="Unduh CSV" />
-												</div>
-											</div>
-										</td>
-									</tr>
+										</p>
+										<Link href={`/rekaman/${item._id}`} className="self-end">
+											<IconComponent
+												icon={<Fullscreen />}
+												name="Lihat Replay"
+											/>
+										</Link>
+									</div>
 								))
 							) : (
-								<tr>
-									<td className="py-2 text-center text-2xl italic md:py-4">
-										Tidak ada data ditemukan
-									</td>
-								</tr>
-							)}
-						</tbody>
-					</table>
-					{/* card for small screen  */}
-					<div className="mt-8 flex w-full flex-col gap-4 px-4 md:hidden">
-						{roadList?.data !== 0 ? (
-							roadList?.data?.map((item, index) => (
-								<div
-									className="flex flex-col gap-1 rounded-md border border-c-blue p-4 shadow-md"
-									key={index}
-								>
-									<p>{convertTime(item.createdAt)}</p>
-									<p className="w-full text-lg font-semibold">{item.title}</p>
-									<p>
-										<span>Jumlah kerusakan: </span>
-										{item.detectionMeta.totalDamage}
-									</p>
-									<Link href={`/rekaman/${item._id}`} className="self-end">
-										<IconComponent icon={<Fullscreen />} name="Lihat Replay" />
-									</Link>
+								<div className="py-2 text-center text-2xl italic md:py-4">
+									Tidak ada data ditemukan
 								</div>
-							))
-						) : (
-							<div className="py-2 text-center text-2xl italic md:py-4">
-								Tidak ada data ditemukan
-							</div>
-						)}
-					</div>
-				</>
-			)}
-		</div>
+							)}
+						</div>
+					</>
+				)}
+			</div>
+			<Tooltip
+				id="icon-tooltip"
+				place="bottom"
+				style={{
+					backgroundColor: '#E0E0E0',
+					color: '#222222',
+					borderRadius: '8px',
+					fontSize: '16px',
+					padding: '4px 12px',
+				}}
+				noArrow
+				className="max-md:hidden"
+			/>
+		</>
 	)
 }
